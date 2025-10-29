1dcbde31e714e4c1dda05f2c0120fae2a0d407cb
Charles-ugo <106807389+Charles-ugo@users.noreply.github.com>
2025-10-29 04:02:48 +0000

    work

---
+++ files/Java.js
@@ -0,0 +1,22 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active")
+    navMenu.classList.toggle("active")
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
+const faqQuestions = document.querySelectorAll('.faq-question');
+faqQuestions.forEach(function(question) {
+    question.addEventListener('click', function() {
+        const answer = question.nextElementSibling;
+        answer.classList.toggle('show');
+    });
+});
+
